c48e1e930c1e17eeee4d1aca61fe7ed22412e317
igorDSMK <igor.nuvem@gmail.com>
2018-03-19 17:24:37 -0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,26 +1,104 @@
-﻿#include "funcao.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include "windows.h"
+#include <iostream>
+#include "math.h"
+#include <iomanip>
+#include <fstream>
 
 using namespace cv;
 using namespace std;
 
+Mat	img, img2;
+int r = 0, g = 0, b = 0;
+
+void r_trackbar(int, void*) {
+	int bb, gg, rr, media;
+	for (int n = 0; n < img.rows; n++) {
+		for (int m = 0; m < img.cols; m++) {
+			bb = img2.at<cv::Vec3b>(n, m)[0];
+			gg = img2.at<cv::Vec3b>(n, m)[1];
+			rr = img2.at<cv::Vec3b>(n, m)[2];
+			media = (bb + rr + gg) / 3;
+			if (rr > media && rr > bb && rr > gg) {
+				if (r < rr) {
+					img.at<cv::Vec3b>(n, m)[0] = (int)media;
+					img.at<cv::Vec3b>(n, m)[1] = (int)media;
+					img.at<cv::Vec3b>(n, m)[2] = (int)media;
+				}
+				else {
+					img.at<cv::Vec3b>(n, m)[0] = img2.at<cv::Vec3b>(n, m)[0];
+					img.at<cv::Vec3b>(n, m)[1] = img2.at<cv::Vec3b>(n, m)[1];
+					img.at<cv::Vec3b>(n, m)[2] = img2.at<cv::Vec3b>(n, m)[2];
+				}
+			}
+		}
+	}
+	imshow("Imagem", img);
+}
+
+void g_trackbar(int, void*) {
+	int bb, gg, rr, media;
+	for (int n = 0; n < img.rows; n++) {
+		for (int m = 0; m < img.cols; m++) {
+			bb = img2.at<cv::Vec3b>(n, m)[0];
+			gg = img2.at<cv::Vec3b>(n, m)[1];
+			rr = img2.at<cv::Vec3b>(n, m)[2];
+			media = (bb + rr + gg) / 3;
+			if (gg > media && gg > rr && gg > bb) {
+				if (g < gg) {
+					img.at<cv::Vec3b>(n, m)[0] = (int)media;
+					img.at<cv::Vec3b>(n, m)[1] = (int)media;
+					img.at<cv::Vec3b>(n, m)[2] = (int)media;
+				}
+				else {
+					img.at<cv::Vec3b>(n, m)[0] = img2.at<cv::Vec3b>(n, m)[0];
+					img.at<cv::Vec3b>(n, m)[1] = img2.at<cv::Vec3b>(n, m)[1];
+					img.at<cv::Vec3b>(n, m)[2] = img2.at<cv::Vec3b>(n, m)[2];
+				}
+			}
+		}
+	}
+	imshow("Imagem", img);
+}
+
+void b_trackbar(int, void*) {
+	int bb, gg, rr, media;
+	for (int n = 0; n < img.rows; n++) {
+		for (int m = 0; m < img.cols; m++) {
+			bb = img2.at<cv::Vec3b>(n, m)[0];
+			gg = img2.at<cv::Vec3b>(n, m)[1];
+			rr = img2.at<cv::Vec3b>(n, m)[2];
+			media = (bb + rr + gg) / 3;
+			if (bb > media && bb > rr && bb > gg) {
+				if (b < bb) {
+					img.at<cv::Vec3b>(n, m)[0] = (int)media;
+					img.at<cv::Vec3b>(n, m)[1] = (int)media;
+					img.at<cv::Vec3b>(n, m)[2] = (int)media;
+				}
+				else {
+					img.at<cv::Vec3b>(n, m)[0] = img2.at<cv::Vec3b>(n, m)[0];
+					img.at<cv::Vec3b>(n, m)[1] = img2.at<cv::Vec3b>(n, m)[1];
+					img.at<cv::Vec3b>(n, m)[2] = img2.at<cv::Vec3b>(n, m)[2];
+				}
+			}
+		}
+	}
+	imshow("Imagem", img);
+}
+
 int main()
 {
-	Mat	im0 = imread("../data/lena.jpg", IMREAD_GRAYSCALE);
-	Mat im2 = imread("../data/lena.jpg", IMREAD_GRAYSCALE);
-	Mat im1 = imread("../data/lena.jpg", IMREAD_GRAYSCALE);
-	/*FAÇA AS CHAMADAS DE FUNÇÃO Q VC QUISER*/	
-
-	Mat im;
-	cv::resize(im1, im, cv::Size(im1.cols, im1.rows / 2), INTER_AREA);	 //operacao do opencv, reduz a "altura" da imagem que será usada na metade
-	
-	string file3 = "../data/output3.txt";
-	writeMatToFile4(im, file3.c_str());
-	string file2 = "../data/output2.txt";
-	writeMatToFile3(im, file2.c_str());
-	string file1 = "../data/output1.txt";
-	writeMatToFile(im, file1.c_str());
-    string file = "../data/output.txt";
-	writeMatToFile2(im, file.c_str());
+	img = imread("caminho da sua imagem", IMREAD_COLOR);  //imagem que será apresentada
+	img2 = imread("caminho da sua imagem", IMREAD_COLOR); //mesma imagem, será usada como referencia 
+							  //e seus dados nunca devem ser alterados
+	namedWindow("Imagem", 1);
+	createTrackbar("R", "Imagem", &r, 255, r_trackbar);
+	createTrackbar("G", "Imagem", &g, 255, g_trackbar);
+	createTrackbar("B", "Imagem", &b, 255, b_trackbar);
+	imshow("Imagem", img);
 
 	waitKey(0);
 	return 0;
